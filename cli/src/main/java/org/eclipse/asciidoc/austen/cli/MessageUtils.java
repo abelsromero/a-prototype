aaa365922a1457fc7bbf2e9cff08948bfdca3785
Abel Salgado Romero <abelromero@gmail.com>
2023-10-08 22:21:31 +0200

    Basic project layout
    TODO: define packages

---
+++ cli/src/main/java/org/eclipse/asciidoc/austen/cli/MessageUtils.java
@@ -0,0 +1,7 @@
+package org.eclipse.asciidoc.austen.cli;
+
+class MessageUtils {
+    static String getMessage() {
+        return "Hello World!";
+    }
+}
---
+++ cli/src/test/java/org/eclipse/asciidoc/austen/cli/MessageUtilsTest.java
@@ -0,0 +1,13 @@
+package org.eclipse.asciidoc.austen.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MessageUtilsTest {
+
+    @Test
+    void testGetMessage() {
+        assertEquals("Hello World!", MessageUtils.getMessage());
+    }
+}
